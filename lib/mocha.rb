b0824ce1db11e62d87903aca6e66795982302a10
Marcus Gartner <marcus@fanium.com>
2012-11-28 12:27:27 -0500

    start of rubymotion port

--- lib/mocha.rb
+++ lib/mocha.rb
@@ -1,8 +1,17 @@
 require 'mocha/version'
 require 'mocha/integration'
 require 'mocha/deprecation'
-
-Mocha::Deprecation.warning("Change `require 'mocha'` to `require 'mocha/setup'`.")
-
 require 'mocha/setup'
 
+unless defined?(Motion::Project::Config)
+  raise "This file must be required within a RubyMotion project Rakefile."
+end
+
+gem_name = File.basename(__FILE__).gsub(/\.rb$/, "")
+
+Motion::Project::App.setup do |app|
+  app.development do
+    gem_files = Dir.glob(File.join(File.dirname(__FILE__), gem_name, "**/*.rb"))
+    app.files = gem_files + app.files
+  end
+end
